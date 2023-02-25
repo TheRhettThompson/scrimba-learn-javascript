9d9d0bc39863b6cb2b4aa6df65d5a5744f283f17
TheRhettThompson <rthompsoncareer@gmail.com>
2023-02-25 23:19:12 +0000

    added metrorail img

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import metrorail from '../../img/metrorail.jpg';
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
